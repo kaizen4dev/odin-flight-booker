770fb0470dfd6c90e8ad034f6b4ce4f9c14535e3
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-06 00:23:04 +0200

    Create flight seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@
   ["NYC", "SFO", "UOE", "HNT", "TML", "KOR"].each do |airport_code|
     Airport.find_or_create_by! code: airport_code
   end
+
+  Airport.first(6).each do |departure_airport|
+    Airport.first(6).each do |arrival_airport|
+      next if departure_airport.id == arrival_airport.id
+      start_time = Time.now + rand(30).days + rand(24).hours
+      duration = "#{rand(12)} hour(s)"
+
+      Flight.find_or_create_by! arrival_airport: arrival_airport, departure_airport: departure_airport, start: start_time, duration: duration
+    end
+  end
