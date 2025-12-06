ee101eeec029be5e435e28714c5528c80e8c1e76
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-06 23:12:44 +0200

    Add days class method for Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
+
+  def self.days
+    select("start").map { |f| f.start.strftime "%Y-%m-%d" }.uniq.sort
+  end
 end
