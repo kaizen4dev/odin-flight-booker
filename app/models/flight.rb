f79f9ce2da258060f423452fc71dabace987bd38
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-07 00:04:51 +0200

    Add default scope for Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
 
+  default_scope { order :start }
+
   def self.days
     select("start").map { |f| f.start.strftime "%Y-%m-%d" }.uniq.sort
   end
