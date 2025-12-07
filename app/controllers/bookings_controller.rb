2248d1797c9445a9c0caebc77019798eb389c805
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-08 00:34:37 +0200

    Define bookings#new controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,9 @@ class BookingsController < ApplicationController
   def show
     @booking = Booking.find params[:id]
   end
+
+  def new
+    @booking = Booking.new
+    params[:passengers].to_i.times { @booking.passengers.new }
+  end
 end
