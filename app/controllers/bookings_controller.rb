c454aecab849b03f89673ad3fb5b0529a526c7fa
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-08 01:23:50 +0200

    Define bookings#create controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,20 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     params[:passengers].to_i.times { @booking.passengers.new }
   end
+
+  def create
+    @booking = Booking.new booking_params
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.expect booking: [ :flight_id, passengers_attributes: [[ :name, :email ]] ]
+  end
 end
