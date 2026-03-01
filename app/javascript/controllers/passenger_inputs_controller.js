c84c87282c32d83986f5e9327df51140782a1022
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-01 21:34:56 +0200

    refactor: use templates for passenger inputs

--- app/javascript/controllers/passenger_inputs_controller.js
+++ app/javascript/controllers/passenger_inputs_controller.js
@@ -2,10 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger-inputs"
 export default class extends Controller {
+  static values = {index: Number};
+  static targets = ["nameLabel", "nameInput", "emailLabel", "emailInput"];
+
   connect() {
+    if(this.element.children.length == 0) {
+      this.removed()
+    }
   }
 
-  remove() {
-    this.element.remove()
+  removed() {
+    const template = document.querySelector("#add-passenger-button-template")
+    const clone = document.importNode(template.content, true)
+    this.element.replaceChildren(clone)
+  }
+
+  added() {
+    const template = document.querySelector("#passenger-inputs-template")
+    const clone = document.importNode(template.content, true)
+    this.element.replaceChildren(clone)
+
+    this.nameLabelTarget.htmlFor = `booking_passengers_attributes_${this.indexValue}_name`
+    this.nameInputTarget.id = `booking_passengers_attributes_${this.indexValue}_name`
+    this.nameInputTarget.name = `booking[passengers_attributes][${this.indexValue}][name]`
+
+    this.emailLabelTarget.htmlFor = `booking_passengers_attributes_${this.indexValue}_email`
+    this.emailInputTarget.id = `booking_passengers_attributes_${this.indexValue}_email`
+    this.emailInputTarget.name = `booking[passengers_attributes][${this.indexValue}][email]`
   }
 }
--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -3,29 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="passengers"
 export default class extends Controller {
   connect() {
-  }
-
-  new() {
-    let number = this.element.children.length
-    if (number < 5) {
+    while (this.element.children.length < 4) {
+      let number = this.element.children.length
       let element = document.createElement("div")
 
-      console.log(this.element.children)
-      this.element.prepend(element)
+      this.element.append(element)
 
       element.outerHTML = `
-        <div data-controller="passenger-inputs">
-          <div>
-            <label for="booking_passengers_attributes_${number}_name">Name</label>
-            <input type="text" name="booking[passengers_attributes][${number}][name]" id="booking_passengers_attributes_${number}_name">
-          </div>
-          <div>
-            <label for="booking_passengers_attributes_${number}_email">Email</label>
-            <input type="email" name="booking[passengers_attributes][${number}][email]" id="booking_passengers_attributes_${number}_email">
-          </div>
-          <button name="button" type="button" data-action="passenger-inputs#remove">Remove Passenger</button>
-          <hr>
-        </div>`
+        <div data-controller="passenger-inputs" data-passenger-inputs-index-value="${number}">
+        </div>
+      `
     }
   }
 }
