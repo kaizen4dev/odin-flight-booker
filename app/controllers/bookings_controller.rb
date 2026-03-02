8ba2715daa20442c8a7020fe9301dba767866fe3
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-02 21:14:56 +0200

    Set up mailer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,6 +12,10 @@ class BookingsController < ApplicationController
     @booking = Booking.new booking_params
 
     if @booking.save
+      @booking.passengers.each do |p|
+        PassengerMailer.with(passenger: p).confirmation_email.deliver_now
+      end
+
       redirect_to @booking
     else
       render :new, status: :unprocessable_entity
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: "no-reply@example.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @booking = @passenger.booking
+    mail(to: @passenger.email, subject: "You have successfully booked a flight!")
+  end
 end
